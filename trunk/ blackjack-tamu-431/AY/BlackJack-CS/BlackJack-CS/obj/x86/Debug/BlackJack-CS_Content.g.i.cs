cf5a719b0b222316f9af4c408f672ddad1c27afc
kevin1188neo@gmail.com <kevin1188neo@gmail.com@6bcca593-157c-048b-4213-1c04b514f038>
2012-04-19 14:56:29 +0000

    Added a player page. no code is currently running. Added to the main screen as well. Need to move the "BlackJack" title away from the cards without erroring the code

--- trunk/ blackjack-tamu-431/AY/BlackJack-CS/BlackJack-CS/obj/x86/Debug/BlackJack-CS_Content.g.i.cs
+++ trunk/ blackjack-tamu-431/AY/BlackJack-CS/BlackJack-CS/obj/x86/Debug/BlackJack-CS_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/chip%20-%20back.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/spades-a-75.png")]
 
 
