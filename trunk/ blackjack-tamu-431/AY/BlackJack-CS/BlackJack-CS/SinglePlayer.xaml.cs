add8c330717e0edd42173347f0082d506c61b014
kevin1188neo@gmail.com <kevin1188neo@gmail.com@6bcca593-157c-048b-4213-1c04b514f038>
2012-04-19 15:29:08 +0000

    Forgot to add the new files... Hope it worked now

---
+++ trunk/ blackjack-tamu-431/AY/BlackJack-CS/BlackJack-CS/SinglePlayer.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackJack_CS
+{
+    /// <summary>
+    /// Interaction logic for SinglePlayer.xaml
+    /// </summary>
+    public partial class SinglePlayer : Page
+    {
+        public SinglePlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void hitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void standButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void doubleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void splitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
